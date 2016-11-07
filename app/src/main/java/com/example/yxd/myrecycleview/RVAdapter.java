40b28df507cbfb11b820669d26349bf114ecb311
King-young <578081020@qq.com>
2016-11-07 16:48:21 +0800

    layout

--- app/src/main/java/com/example/yxd/myrecycleview/RVAdapter.java
+++ app/src/main/java/com/example/yxd/myrecycleview/RVAdapter.java
@@ -8,6 +8,9 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Yxd on 2016/11/3.
  */
@@ -15,12 +18,26 @@ public class RVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     private Context mContex;
     private LayoutInflater inflater;
-    private String[] itemStrings={"aaaaa","bbbbb","ccccc","ddddd","eeeee","fffff","ggggg","hhhhh"};
+    private List<String> mDatas;
     private MyOnItemClickListener onItemClickListener;
 
     public RVAdapter(Context mContex) {
         this.mContex = mContex;
         this.inflater = LayoutInflater.from(mContex);
+        mDatas=new ArrayList<>();
+        mDatas.add("aa");
+        mDatas.add("qq");
+        mDatas.add("ww");
+        mDatas.add("ee");
+        mDatas.add("rr");
+        mDatas.add("tt");
+        mDatas.add("yy");
+        mDatas.add("uu");
+        mDatas.add("ii");
+        mDatas.add("oo");
+        mDatas.add("pp");
+        mDatas.add("ss");
+        mDatas.add("dd");
     }
 
     public void setOnItemClickListener(MyOnItemClickListener onItemClickListener) {
@@ -35,7 +52,23 @@ public class RVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
         }
         return 2;
     }
+    int a=0;
+    public void addData(int position) {
 
+        mDatas.add(position, "Insert"+a);
+        notifyItemInserted(position);                       //
+        notifyItemRangeChanged(position,mDatas.size()+1);
+        notifyDataSetChanged();
+        a++;
+    }
+
+    public void removeData(int position) {
+        mDatas.remove(position-1);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position,mDatas.size()+1);
+        notifyDataSetChanged();
+
+    }
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         switch (viewType)
@@ -54,7 +87,6 @@ public class RVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
         }
     }
-
     @Override
     public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
         if (getItemViewType(position)==1 && position==0)
@@ -64,17 +96,19 @@ public class RVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
                 @Override
                 public void onClick(View view) {
                     Toast.makeText(mContex,position+"",Toast.LENGTH_LONG).show();
+                    addData(mDatas.size());
                 }
             });
 
         }
         if (getItemViewType(position)==2 && position!=0)
         {
-            ((MyViewHolder)holder).tv.setText(itemStrings[position-1]);
+            ((MyViewHolder)holder).tv.setText(mDatas.get(position-1));
             ((MyViewHolder)holder).itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Toast.makeText(mContex,position+"",Toast.LENGTH_LONG).show();
+                    removeData(position);
                 }
             });
         }
@@ -83,7 +117,7 @@ public class RVAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
     @Override
     public int getItemCount() {
-        return itemStrings.length+1;
+        return mDatas.size()+1;
     }
     class  MyViewHolder extends RecyclerView.ViewHolder
     {
